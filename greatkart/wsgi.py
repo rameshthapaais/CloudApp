a50e156cf11f57ca85397bf5af6729db90b6b15f
rameshthapaais <rath842@ess.ais.ac.nz>
2024-10-14 20:50:42 +1300

    Added settings module

--- greatkart/wsgi.py
+++ greatkart/wsgi.py
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 settings_module = 'greatkart.production' if 'WEBSITE_HOSTNAME' in os.environ else 'greatkart.settings'
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greatkart.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,8 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'greatkart.settings')
+    settings_module = 'greatkart.production' if 'WEBSITE_HOSTNAME' in os.environ else 'greatkart.settings'
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',settings_module )
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
