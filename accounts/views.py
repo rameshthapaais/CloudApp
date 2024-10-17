40f77472d8add099c370c7ca1bd3408d0abf2f33
rameshthapaais <rath842@ess.ais.ac.nz>
2024-10-17 23:58:56 +1300

    Removed gmail sending mail

--- accounts/views.py
+++ accounts/views.py
@@ -32,24 +32,27 @@ def register(request):
             user = Account.objects.create_user(
                 first_name=first_name, last_name=last_name, email=email, username=username, password=password)
             user.phone_number = phone_number
+            user.is_active = True
             user.save()
 
+            messages.success(request, 'Account created successfully! You can now log in.')
+            return redirect('login')
             
             # USER ACTIVATION
-            current_site = get_current_site(request)
-            mail_subject = 'Please activate your account'
-            message = render_to_string('accounts/account_verification_email.html', {
-                'user': user,
-                'domain': current_site,
-                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                'token': default_token_generator.make_token(user),
-            })
-            to_email = email
+            # current_site = get_current_site(request)
+            # mail_subject = 'Please activate your account'
+            # message = render_to_string('accounts/account_verification_email.html', {
+            #     'user': user,
+            #     'domain': current_site,
+            #     'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+            #     'token': default_token_generator.make_token(user),
+            # })
+            # to_email = email
             
-            send_email = EmailMessage(mail_subject, message, to=[to_email])
-            send_email.send()
-            # messages.success(request,'Account created.Please check the link for activation!')
-            return redirect('/accounts/login?command=verification&email='+email)
+            # send_email = EmailMessage(mail_subject, message, to=[to_email])
+            # send_email.send()
+            # # messages.success(request,'Account created.Please check the link for activation!')
+            # return redirect('/accounts/login?command=verification&email='+email)
     else:
         form = RegistrationForm()
 
